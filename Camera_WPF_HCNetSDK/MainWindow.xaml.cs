a9bcabf61e5d1033ff9370ec327f1f493047c831
Michał Szczygieł <michal@robot4you.pl>
2026-05-08 15:01:21 +0200

    Tymczas
    Prace nad wyświetlaniem obrazu z wszystkich kamer. Na tą chwilę nie wyśiwetla jednej kamery i program sie wyłancza bo jej nie ma fizycznie podpiętej

--- Camera_WPF_HCNetSDK/MainWindow.xaml.cs
+++ Camera_WPF_HCNetSDK/MainWindow.xaml.cs
@@ -29,7 +29,9 @@ namespace Camera_WPF_HCNetSDK
     /// </summary>
     public partial class MainWindow : Window
     {
-        private bool state_playing;
+        private bool state_playing1;
+        private bool state_playing2;
+        private bool state_playing3;
         public int user_id_1 = -1;
         public int user_id_2 = -1;
         public int user_id_3 = -1;
@@ -44,12 +46,20 @@ namespace Camera_WPF_HCNetSDK
         public int play_handle3 = 0;
         public int play_handle4 = 0;
 
-        int m_lPort = -1;
-        private REALDATACALLBACK RealData; // Przechowuj delegata, by GC go nie usunął
+        int m_lPort1 = -1;
+        int m_lPort2 = -1;
+        int m_lPort3 = -1;
+        private REALDATACALLBACK RealData1; // Przechowuj delegata, by GC go nie usunął
+        private REALDATACALLBACK RealData2; // Przechowuj delegata, by GC go nie usunął
+        private REALDATACALLBACK RealData3; // Przechowuj delegata, by GC go nie usunął
+        
         private DECCBFUN m_DecCallback; // Przechowuj delegata callbacku dekodowania
+        
         private byte[] rgbBuffer; // Globalny bufor, by nie tworzyć go co klatkę
 
-        WriteableBitmap wbmp = new WriteableBitmap(2688, 1520, 96, 96, PixelFormats.Bgr24, null);
+        WriteableBitmap wbmp1 = new WriteableBitmap(2688, 1520, 96, 96, PixelFormats.Bgr24, null);
+        WriteableBitmap wbmp2 = new WriteableBitmap(2560, 1440, 96, 96, PixelFormats.Bgr24, null);
+        WriteableBitmap wbmp3 = new WriteableBitmap(2560, 1440, 96, 96, PixelFormats.Bgr24, null);
 
         public MainWindow()
         {
@@ -59,7 +69,7 @@ namespace Camera_WPF_HCNetSDK
 
         private void ButtonStart_Click(object sender, RoutedEventArgs e)
         {
-            if (state_playing)
+            if (state_playing1)
             {
                 NET_DVR_StopRealPlay(play_handle1);
                 NET_DVR_StopRealPlay(play_handle2);
@@ -70,11 +80,17 @@ namespace Camera_WPF_HCNetSDK
                 NET_DVR_Logout(user_id_2);
                 NET_DVR_Logout(user_id_3);
 
-                PlayM4_Stop(m_lPort);
-                PlayM4_CloseStream(m_lPort);
+                PlayM4_Stop(m_lPort1);
+                PlayM4_Stop(m_lPort2);
+                PlayM4_Stop(m_lPort3);
+                PlayM4_CloseStream(m_lPort1);
+                PlayM4_CloseStream(m_lPort2);
+                PlayM4_CloseStream(m_lPort3);
                 ButtonStart.Content = "Start";
 
-                state_playing = false;
+                state_playing1 = false;
+                state_playing2 = false;
+                state_playing3 = false;
             }
             else
             {
@@ -132,21 +148,144 @@ namespace Camera_WPF_HCNetSDK
                     byPreviewMode = 0
                 };
 
-                RealData = new REALDATACALLBACK(RealDataCallback);
-                play_handle1 = NET_DVR_RealPlay_V40(user_id_1, ref lpPreviewInfo1, RealData, pUser1);
-                // Rozpoczęcie podglądu
-                    MainCamera.Source = wbmp;
-                    RightCameraSmall.Source = wbmp;
-                    LeftCameraSmall.Source = wbmp;
-                    CenterCameraSmall.Source = wbmp;
+                RealData1 = new REALDATACALLBACK(RealDataCallback1);
+                RealData2 = new REALDATACALLBACK(RealDataCallback2);
+                RealData3 = new REALDATACALLBACK(RealDataCallback3);
 
-                state_playing = true;
+                play_handle1 = NET_DVR_RealPlay_V40(user_id_1, ref lpPreviewInfo1, RealData1, pUser1);
+                play_handle2 = NET_DVR_RealPlay_V40(user_id_2, ref lpPreviewInfo2, RealData2, pUser2);
+                play_handle3 = NET_DVR_RealPlay_V40(user_id_3, ref lpPreviewInfo3, RealData3, pUser3);
+                
+                // Rozpoczęcie podglądu
+                MainCamera.Source = wbmp1;
+                RightCameraSmall.Source = wbmp2;
+                LeftCameraSmall.Source = wbmp3;
+                CenterCameraSmall.Source = wbmp1;
+
+                state_playing1 = true;
+                state_playing2 = true;
+                state_playing3 = true;
             }
 
         }
-        private void DecCallback(int nPort, IntPtr pBuf, int nSize, ref PlayCtrl.FRAME_INFO pFrameInfo, int nUser, int nReserved2)
+        private void RealDataCallback1(int lRealHandle, uint dwDataType, IntPtr pBuffer, uint dwBufSize, IntPtr pUser)
         {
-            if (m_lPort == -1 || nSize <= 0 || pBuf == IntPtr.Zero)
+            switch (dwDataType)
+            {
+                case NET_DVR_SYSHEAD:
+                    if (!PlayM4_GetPort(ref m_lPort1)) return;
+                    if (dwBufSize > 0)
+                    {
+                        if (PlayM4_OpenStream(m_lPort1, pBuffer, dwBufSize, 1024 * 1024))
+                        {
+                            PlayM4_SetDisplayType(m_lPort1, 0); // Format RGB
+                            m_DecCallback = new DECCBFUN(DecCallback1);
+                            PlayM4_SetDecCallBack(m_lPort1, m_DecCallback);
+
+                            try
+                            {
+                                PlayM4_Play(m_lPort1, IntPtr.Zero);
+                            }
+                            catch (Exception ex)
+                            {
+                                WriteLine("Błąd: " + ex.Message);
+                            }
+                        }
+                    }
+                    break;
+
+                case NET_DVR_STREAMDATA: // Dane wideo
+                    if (m_lPort1 != -1 && dwBufSize > 0)
+                    {
+                        // Przekazanie danych do dekodera
+                        if (!PlayM4_InputData(m_lPort1, pBuffer, dwBufSize))
+                        {
+                            // Ewentualna obsługa błędu przepełnienia bufora
+                        }
+                    }
+                    break;
+            }
+        }
+        private void RealDataCallback2(int lRealHandle, uint dwDataType, IntPtr pBuffer, uint dwBufSize, IntPtr pUser)
+        {
+            switch (dwDataType)
+            {
+                case NET_DVR_SYSHEAD:
+                    if (!PlayM4_GetPort(ref m_lPort2)) return;
+                    if (dwBufSize > 0)
+                    {
+                        if (PlayM4_OpenStream(m_lPort2, pBuffer, dwBufSize, 1024 * 1024))
+                        {
+                            PlayM4_SetDisplayType(m_lPort2, 0); // Format RGB
+                            m_DecCallback = new DECCBFUN(DecCallback2);
+                            PlayM4_SetDecCallBack(m_lPort2, m_DecCallback);
+
+                            try
+                            {
+                                PlayM4_Play(m_lPort2, IntPtr.Zero);
+                            }
+                            catch (Exception ex)
+                            {
+                                WriteLine("Błąd: " + ex.Message);
+                            }
+                        }
+                    }
+                    break;
+
+                case NET_DVR_STREAMDATA: // Dane wideo
+                    if (m_lPort2 != -1 && dwBufSize > 0)
+                    {
+                        // Przekazanie danych do dekodera
+                        if (!PlayM4_InputData(m_lPort2, pBuffer, dwBufSize))
+                        {
+                            // Ewentualna obsługa błędu przepełnienia bufora
+                        }
+                    }
+                    break;
+            }
+        }
+        private void RealDataCallback3(int lRealHandle, uint dwDataType, IntPtr pBuffer, uint dwBufSize, IntPtr pUser)
+        {
+            switch (dwDataType)
+            {
+                case NET_DVR_SYSHEAD:
+                    if (!PlayM4_GetPort(ref m_lPort3)) return;
+                    if (dwBufSize > 0)
+                    {
+                        if (PlayM4_OpenStream(m_lPort3, pBuffer, dwBufSize, 1024 * 1024))
+                        {
+                            PlayM4_SetDisplayType(m_lPort3, 0); // Format RGB
+                            m_DecCallback = new DECCBFUN(DecCallback3);
+                            PlayM4_SetDecCallBack(m_lPort3, m_DecCallback);
+
+                            try
+                            {
+                                PlayM4_Play(m_lPort3, IntPtr.Zero);
+                            }
+                            catch (Exception ex)
+                            {
+                                WriteLine("Błąd: " + ex.Message);
+                            }
+                        }
+                    }
+                    break;
+
+                case NET_DVR_STREAMDATA: // Dane wideo
+                    if (m_lPort3 != -1 && dwBufSize > 0)
+                    {
+                        // Przekazanie danych do dekodera
+                        if (!PlayM4_InputData(m_lPort3, pBuffer, dwBufSize))
+                        {
+                            // Ewentualna obsługa błędu przepełnienia bufora
+                        }
+                    }
+                    break;
+            }
+        }
+
+        private void DecCallback1(int nPort, IntPtr pBuf, int nSize, ref PlayCtrl.FRAME_INFO pFrameInfo, int nUser, int nReserved2)
+        {
+            if (m_lPort1 == -1 || nSize <= 0 || pBuf == IntPtr.Zero)
                 return;
 
             int width = pFrameInfo.nWidth;
@@ -161,15 +300,15 @@ namespace Camera_WPF_HCNetSDK
             // Przesyłamy do wątku UI
             Application.Current.Dispatcher.BeginInvoke(new Action(() =>
             {
-                if (!state_playing || wbmp == null)
+                if (!state_playing1 || wbmp1 == null)
                     return;
 
                 try
                 {
-                    wbmp.Lock();
+                    wbmp1.Lock();
                     // Kopiujemy już gotowe dane RGB do naszej bitmapy WPF
-                    ConvertYV12ToRGB(pBuf, wbmp.BackBuffer, width, height);
-                    wbmp.AddDirtyRect(new Int32Rect(0, 0, width, height));
+                    ConvertYV12ToRGB(pBuf, wbmp1.BackBuffer, width, height);
+                    wbmp1.AddDirtyRect(new Int32Rect(0, 0, width, height));
                     Console.WriteLine("test");
                 }
                 catch (Exception ex)
@@ -178,48 +317,85 @@ namespace Camera_WPF_HCNetSDK
                 }
                 finally
                 {
-                    wbmp.Unlock();
+                    wbmp1.Unlock();
                 }
             }));
         }
-
-        private void RealDataCallback(int lRealHandle, uint dwDataType, IntPtr pBuffer, uint dwBufSize, IntPtr pUser)
+        private void DecCallback2(int nPort, IntPtr pBuf, int nSize, ref PlayCtrl.FRAME_INFO pFrameInfo, int nUser, int nReserved2)
         {
-            switch (dwDataType)
+            if (m_lPort2 == -1 || nSize <= 0 || pBuf == IntPtr.Zero)
+                return;
+
+            int width = pFrameInfo.nWidth;
+            int height = pFrameInfo.nHeight;
+            int expectedRgbSize = width * height * 3;
+
+            // Inicjalizacja bufora raz, przy zmianie rozdzielczości
+            if (rgbBuffer == null || rgbBuffer.Length != expectedRgbSize)
             {
-                case NET_DVR_SYSHEAD:
-                    if (!PlayM4_GetPort(ref m_lPort)) return;
-                    if (dwBufSize > 0)
-                    {
-                        if (PlayM4_OpenStream(m_lPort, pBuffer, dwBufSize, 1024 * 1024))
-                        {
-                            PlayM4_SetDisplayType(m_lPort, 0); // Format RGB
-                            m_DecCallback = new DECCBFUN(DecCallback);
-                            PlayM4_SetDecCallBack(m_lPort, m_DecCallback);
-
-                            try
-                            {
-                                PlayM4_Play(m_lPort, IntPtr.Zero);
-                            }
-                            catch (Exception ex)
-                            {
-                                WriteLine("Błąd: " + ex.Message);
-                            }
-                        }
-                    }
-                    break;
-
-                case NET_DVR_STREAMDATA: // Dane wideo
-                    if (m_lPort != -1 && dwBufSize > 0)
-                    {
-                        // Przekazanie danych do dekodera
-                        if (!PlayM4_InputData(m_lPort, pBuffer, dwBufSize))
-                        {
-                            // Ewentualna obsługa błędu przepełnienia bufora
-                        }
-                    }
-                    break;
+                rgbBuffer = new byte[expectedRgbSize];
             }
+            // Przesyłamy do wątku UI
+            Application.Current.Dispatcher.BeginInvoke(new Action(() =>
+            {
+                if (!state_playing2 || wbmp2 == null)
+                    return;
+
+                try
+                {
+                    wbmp2.Lock();
+                    // Kopiujemy już gotowe dane RGB do naszej bitmapy WPF
+                    ConvertYV12ToRGB(pBuf, wbmp2.BackBuffer, width, height);
+                    wbmp2.AddDirtyRect(new Int32Rect(0, 0, width, height));
+                    Console.WriteLine("test");
+                }
+                catch (Exception ex)
+                {
+                    WriteLine("Błąd: " + ex.Message);
+                }
+                finally
+                {
+                    wbmp2.Unlock();
+                }
+            }));
+        }
+        private void DecCallback3(int nPort, IntPtr pBuf, int nSize, ref PlayCtrl.FRAME_INFO pFrameInfo, int nUser, int nReserved2)
+        {
+            if (m_lPort3 == -1 || nSize <= 0 || pBuf == IntPtr.Zero)
+                return;
+
+            int width = pFrameInfo.nWidth;
+            int height = pFrameInfo.nHeight;
+            int expectedRgbSize = width * height * 3;
+
+            // Inicjalizacja bufora raz, przy zmianie rozdzielczości
+            if (rgbBuffer == null || rgbBuffer.Length != expectedRgbSize)
+            {
+                rgbBuffer = new byte[expectedRgbSize];
+            }
+            // Przesyłamy do wątku UI
+            Application.Current.Dispatcher.BeginInvoke(new Action(() =>
+            {
+                if (!state_playing3 || wbmp3 == null)
+                    return;
+
+                try
+                {
+                    wbmp3.Lock();
+                    // Kopiujemy już gotowe dane RGB do naszej bitmapy WPF
+                    ConvertYV12ToRGB(pBuf, wbmp3.BackBuffer, width, height);
+                    wbmp3.AddDirtyRect(new Int32Rect(0, 0, width, height));
+                    Console.WriteLine("test");
+                }
+                catch (Exception ex)
+                {
+                    WriteLine("Błąd: " + ex.Message);
+                }
+                finally
+                {
+                    wbmp3.Unlock();
+                }
+            }));
         }
 
         private void ConvertYV12ToRGB(IntPtr pBuf, IntPtr pDest, int width, int height)
